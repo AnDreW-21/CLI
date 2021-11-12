c5d9329c8fe5ab195d28e12f9de7a8d092dcc97b
mAshrafDawood <ashraf.dawood2295@gmail.com>
2021-11-12 16:33:37 +0200

    moved Parser class to Terminal.java

--- src/Terminal.java
+++ src/Terminal.java
@@ -3,6 +3,48 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.util.Scanner;
+import java.util.Arrays;
+
+
+class Parser {
+
+    String commandName;
+    String[] args;
+    boolean toFile;
+    boolean appendToFile;
+    String outputFilePath;
+
+
+    public boolean parse(String input){
+        String[] temp = input.split(" ");
+        commandName = temp[0];
+        args = Arrays.copyOfRange(temp, 1, temp.length);
+        toFile = input.contains(">");
+        appendToFile = input.contains(">>");
+
+        if (appendToFile){
+            outputFilePath = input.split(">>")[1];
+            outputFilePath = outputFilePath.strip();
+        }
+        else if (toFile){
+            outputFilePath = input.split(">")[1];
+            outputFilePath = outputFilePath.strip();
+        }
+
+
+
+        return true;
+    }
+
+    public String getCommandName(){
+        return commandName;
+    }
+
+    public String[] getArgs(){
+        return args;
+    }
+}
+
 
 public class Terminal {
     Parser parser;
